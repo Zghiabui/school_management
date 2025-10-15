800ba4995ba145f8aa21125b3e74135f7404a7e4
Zghiabui <buitrongnghiaaaaa@gmail.com>
2025-10-16 00:31:14 +0700

    first commit

---
+++ src/main/java/com/example/schoolmanagmentrsystem/SchoolManagmentrSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolmanagmentrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagmentrSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagmentrSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolmanagmentrsystem/SchoolManagmentrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolmanagmentrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentrSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
