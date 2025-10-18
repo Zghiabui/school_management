8a7497ca600ed7dd6587539a018468ec62635b8f
Zghiabui <buitrongnghiaaaaa@gmail.com>
2025-10-18 17:41:47 +0700

    FacultyDTO

--- src/main/java/com/example/schoolmanagmentsystem/dto/FacultyDTO.java
+++ src/main/java/com/example/schoolmanagmentsystem/dto/FacultyDTO.java
@@ -1,4 +1,18 @@
 package com.example.schoolmanagmentsystem.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class FacultyDTO {
+    private Long facultyId;
+
+    @NotBlank(message = "Tên danh mục không được để trống")
+    private String facultyName;
+    private String dean;
+    private String phone;
+    private String email;
+    private String description;
+    private String address;
+
 }
