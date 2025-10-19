87e0182df2a836029695e068e2029c7788785085
nkminh14 <ngyenkhoaminh0104@gmail.com>
2025-10-19 20:17:33 +0700

    phan diem

--- src/main/java/com/example/schoolmanagmentsystem/repository/GradeRepository.java
+++ src/main/java/com/example/schoolmanagmentsystem/repository/GradeRepository.java
@@ -1,4 +1,9 @@
 package com.example.schoolmanagmentsystem.repository;
 
-public interface GradeRepository {
+import com.example.schoolmanagmentsystem.entity.Grade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GradeRepository extends JpaRepository<Grade, Long> {
 }
