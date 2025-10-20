0650275fe073e565a096da84d10cfd6f7e434d15
nkminh14 <ngyenkhoaminh0104@gmail.com>
2025-10-20 09:03:09 +0700

    sua lai Tuition

--- src/main/java/com/example/schoolmanagmentsystem/controller/TuitionController.java
+++ src/main/java/com/example/schoolmanagmentsystem/controller/TuitionController.java
@@ -1,4 +1,27 @@
 package com.example.schoolmanagmentsystem.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.schoolmanagmentsystem.dto.TuitionDTO;
+import com.example.schoolmanagmentsystem.service.TuitionService;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/tuitions")
 public class TuitionController {
+
+    @Autowired
+    private TuitionService tuitionService;
+
+    @GetMapping
+    public List<TuitionDTO> getAllTuitions() {
+        return tuitionService.getAllTuitions();
+    }
+
 }
